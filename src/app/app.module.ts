0bdf614fbc1bda02a98bff12ef9d8135d177b4e6
Dan Cornilov <dancornilov@gmail.com>
2023-07-21 17:18:38 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IConfig, NgxMaskDirective, provideEnvironmentNgxMask } from 'ngx-mask';
+import { ReactiveFormsModule } from '@angular/forms';
 
+
+const maskConfig: Partial<IConfig> = {
+  showMaskTyped: true
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMaskDirective,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    provideEnvironmentNgxMask(maskConfig)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
